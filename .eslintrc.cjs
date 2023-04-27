0e7713693a6ae633a56e56c12981aaa62dd0a495
il <wer4272@nate.com>
2023-04-27 17:29:02 +0900

    feat: storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true
+  },
   node: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": "warn",
-  },
+    "react-refresh/only-export-components": "warn"
+  }
 };
